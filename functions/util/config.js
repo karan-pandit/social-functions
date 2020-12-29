1f2315b26e2b01794a75d729dc487ac653f4f6ae
Karan Pandit <kp@Karans-MacBook-Pro.local>
2020-12-29 13:34:59 +1100

    setup api

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCY3JNZArOXyn7tLeQg1j_JGwI6vqm01II",
+    authDomain: "social-1db31.firebaseapp.com",
+    projectId: "social-1db31",
+    storageBucket: "social-1db31.appspot.com",
+    messagingSenderId: "381375774126",
+    appId: "1:381375774126:web:cd574e973d5c6d74611c3f",
+    measurementId: "G-LX6TNJW21Y"
+  };
